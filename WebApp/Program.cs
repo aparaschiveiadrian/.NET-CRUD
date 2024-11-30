a133c44a87f5cb710a1a24a109ee11639c91e3fe
Aparaschivei Adrian <adrian.aparaschivei04@gmail.com>
2024-12-01 00:58:44 +0200

    added bootstrap+ use of static file middleware

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -5,6 +5,9 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+//Step 4: Bootstrap, will be able to go into wwwrooot to locate the static file that is requested
+app.UseStaticFiles();
+
 //we need to add the routing capabilities(it maps the request to the method inside the controller)
 //Step1: enable MVC framework
 app.UseRouting();
